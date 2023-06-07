505849424f67d621608bed9b7e84be735215f419
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-06-07 22:01:19 +0800

    add vercel to cors

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173","https://ml-fruit-freshness-classifier.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
