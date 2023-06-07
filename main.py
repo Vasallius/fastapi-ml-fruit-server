7dfccf0457e51c9e83b4c7c80ae23d255848c1a4
Jedidiah Carl O. Tan <masterjed7262@gmail.com>
2023-06-07 16:17:24 +0800

    add CORS

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from pydantic import BaseModel
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 class Msg(BaseModel):
     msg: str
